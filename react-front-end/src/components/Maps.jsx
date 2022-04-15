55399d8153bfca29da8bc07bfb91c771934c4bce
Ryan <ryan.maceachern@gmail.com>
2022-04-15 16:03:09 -0700

    env variable now accessed correctly

--- react-front-end/src/components/Maps.jsx
+++ react-front-end/src/components/Maps.jsx
@@ -1,15 +1,16 @@
-import { useState, useMemo } from 'react';
+import { useMemo } from 'react';
 import { GoogleMap, useJsApiLoader, useLoadScript, Marker } from "@react-google-maps/api";
 
 export default function Maps() {
   const { isLoaded } = useJsApiLoader({
     id: 'google-map-script',
-    googleMapsApiKey: process.env.GOOGLE_MAPS_API_KEY 
+    googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAPS_API_KEY 
   })
-  const [map, setMap] = useState(null)
+
+  console.log("API KEY:", process.env.REACT_APP_GOOGLE_MAPS_API_KEY)
   
   if (!isLoaded) return <div>Loading...</div>
-  
+
   return <GoogleMap 
   zoom={10} 
   center={{lat: 44, lng: -80}}
