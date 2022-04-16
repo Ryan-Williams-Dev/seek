3f06720a16747de08fec7232dadaa38b11141433
Zenophage <79440234+Zenophage@users.noreply.github.com>
2022-04-16 16:52:05 -0700

    got the server hooked up to the db, and modified reset db file to auto drop, remake, and reseed db

--- express-back-end/server.js
+++ express-back-end/server.js
@@ -1,11 +1,22 @@
+require("dotenv").config({silent: true});
+
 const Express = require('express');
 const App = Express();
 const BodyParser = require('body-parser');
-const PORT = 8080;
+const PORT = process.env.PORT || 8080;
 
 // DB setup
-// const { Pool } = require("pg")
-// const dbParams = require("./lib/db.js")
+const { Pool } = require("pg")
+const dbParams = require("./lib/db.js")
+const db = new Pool(dbParams);
+db.connect();
+// Test Query
+db.query('SELECT * FROM users LIMIT 1;')
+  .then((res) =>  {
+    arr = Object.values(res.rows)
+    console.log("Database succesfully connected, here's a user as proof...: ", arr)
+  })
+  .catch(err => console.log("Unable to connect to the database. Error: " + err));
 
 // Express Configuration
 App.use(BodyParser.urlencoded({ extended: false }));
