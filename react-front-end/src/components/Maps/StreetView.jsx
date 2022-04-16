d4708de1d57785b72dfb25dc26477e4005acb758
Ryan <ryan.maceachern@gmail.com>
2022-04-16 12:59:31 -0700

    remove defaultUI from streetview map

--- react-front-end/src/components/Maps/StreetView.jsx
+++ react-front-end/src/components/Maps/StreetView.jsx
@@ -1,17 +1,22 @@
-import { GoogleMap, useJsApiLoader, useLoadScript, Marker } from "@react-google-maps/api";
+import { GoogleMap, useJsApiLoader } from "@react-google-maps/api";
 import { StreetViewPanorama } from "@react-google-maps/api";
 
 export default function StreetView() {
-  const { isLoaded } = useJsApiLoader({
+  const { isLoaded, loadError } = useJsApiLoader({
     id: 'google-map-script',
     googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAPS_API_KEY 
   })
 
   if (!isLoaded) return <div>Loading...</div>
+  if (loadError) return `Error loading maps: ${loadError}`;
 
+  const options = {
+    disableDefaultUI: true
+  }
   return <StreetViewPanorama 
     position={{lat: 48.373229, lng:-123.586959}}
     visible={true}
+    options={options}
   ></StreetViewPanorama>
 
 }
