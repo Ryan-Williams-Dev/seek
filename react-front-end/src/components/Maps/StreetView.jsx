8347bc835eb7051ed7c564e748c5ac4a7e839b69
Ryan <ryan.maceachern@gmail.com>
2022-04-15 20:49:54 -0700

    linting changes

--- react-front-end/src/components/Maps/StreetView.jsx
+++ react-front-end/src/components/Maps/StreetView.jsx
@@ -1,5 +1,6 @@
 import { GoogleMap, useJsApiLoader, useLoadScript, Marker } from "@react-google-maps/api";
 
+
 export default function StreetView() {
   const { isLoaded } = useJsApiLoader({
     id: 'google-map-script',
@@ -9,7 +10,8 @@ export default function StreetView() {
   if (!isLoaded) return <div>Loading...</div>
 
   return <GoogleMap 
-  zoom={10} 
+  zoom={10}
+  streetView="false"
   center={{lat: 48.373223, lng: -123.586957}}
   mapContainerClassName="map-container"
   ></GoogleMap>
