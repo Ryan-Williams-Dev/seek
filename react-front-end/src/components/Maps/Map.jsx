342146d068544ad20e2e7c02f5b52f42e638cd67
Ryan <ryan.maceachern@gmail.com>
2022-04-16 21:20:33 -0700

    can now add multiple markers to the map

--- react-front-end/src/components/Maps/Map.jsx
+++ react-front-end/src/components/Maps/Map.jsx
@@ -1,13 +1,15 @@
-import { GoogleMap, useJsApiLoader } from "@react-google-maps/api";
+import { useState } from 'react';
+import { GoogleMap, useJsApiLoader, Marker } from "@react-google-maps/api";
 import mapStyles from "../../mapStyles";
 
 export default function Map() {
+  const [markers, setMarkers] = useState([]);
   const { isLoaded, loadError } = useJsApiLoader({
     id: 'google-map-script',
     googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAPS_API_KEY 
-  })
+  });
 
-  if (!isLoaded) return <div>Loading...</div>
+  if (!isLoaded) return <div>Loading...</div>;
   if (loadError) return `Error loading maps: ${loadError}`;
 
   const options = {
@@ -15,10 +17,25 @@ export default function Map() {
   }
 
   return <GoogleMap 
-  zoom={2.5}
-  center={{lat: 50, lng: 50}}
-  mapContainerClassName="map-container"
-  options={options}
-  ></GoogleMap>
+    zoom={2.5}
+    center={{lat: 50, lng: 50}}
+    mapContainerClassName="map-container"
+    options={options}
+    onClick={(event) => {
+      setMarkers( current => [...current, {
+        lat: event.latLng.lat(),
+        lng: event.latLng.lng(),
+        time: new Date()
+      }])
+      console.log("Markers:", markers)
+    }}
+  >
+    {markers.map((marker) => (
+    <Marker 
+      key={marker.time.toISOString()}
+      position={{ lat: marker.lat, lng: marker.lng }}
+    />
+    ))}
+  </GoogleMap>
 
 }
