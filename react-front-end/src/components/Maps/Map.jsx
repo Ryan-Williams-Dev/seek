8abe41bdfb18487e02bc45e6a73d5459fd7d6b89
Ryan <ryan.maceachern@gmail.com>
2022-04-16 17:32:46 -0700

    fix for page loading before API was loaded

--- react-front-end/src/components/Maps/Map.jsx
+++ react-front-end/src/components/Maps/Map.jsx
@@ -1,13 +1,19 @@
-import { GoogleMap, useJsApiLoader, useLoadScript, Marker } from "@react-google-maps/api";
+/* global google */
+/* eslint-disable no-undef */
+import { GoogleMap, useJsApiLoader } from "@react-google-maps/api";
 import mapStyles from "../../mapStyles";
 
+const google = window.google;
+
 export default function Map() {
-  const { isLoaded } = useJsApiLoader({
+  const { isLoaded, loadError } = useJsApiLoader({
     id: 'google-map-script',
     googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAPS_API_KEY 
   })
 
   if (!isLoaded) return <div>Loading...</div>
+  if (loadError) return `Error loading maps: ${loadError}`;
+
 
   const options = {
     styles: mapStyles
