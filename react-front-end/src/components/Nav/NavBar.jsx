b85dbc04ff861e83712071fee7bed7ba559373ef
Zenophage <79440234+Zenophage@users.noreply.github.com>
2022-04-17 09:37:18 -0700

    got the drawer appearing and dissapearing appropriatly, next is to make the list

--- react-front-end/src/components/Nav/NavBar.jsx
+++ react-front-end/src/components/Nav/NavBar.jsx
@@ -1,35 +1,41 @@
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
-import Button from '@mui/material/Button';
-import IconButton from '@mui/material/IconButton';
+import { useState } from 'react';
+import { AppBar, Box, Toolbar, Typography, Button, IconButton, Drawer } from '@mui/material';
 import MenuIcon from '@mui/icons-material/Menu';
 
 export default function ButtonAppBar(props) {
 
-  
+  const [drawerOpen, setDrawerOpen] = useState(false);
 
+  const toggleDrawer = (open) => (event) => {
+    setDrawerOpen(open);
+  };
 
   return (
-    <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static">
-        <Toolbar>
-          <IconButton
-            size="large"
-            edge="start"
-            color="inherit"
-            aria-label="menu"
-            sx={{ mr: 2 }}
-          >
-            <MenuIcon />
-          </IconButton>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            SEEK
-          </Typography>
-          <Button color="inherit">Login</Button>
-        </Toolbar>
-      </AppBar>
-    </Box>
+    <>
+      <Box sx={{ flexGrow: 1 }}>
+        <AppBar position="static">
+          <Toolbar>
+            <IconButton
+              size="large"
+              edge="start"
+              color="inherit"
+              aria-label="menu"
+              sx={{ mr: 2 }}
+              onClick={toggleDrawer(true)}
+            >
+              <MenuIcon />
+            </IconButton>
+            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+              SEEK
+            </Typography>
+            <Button color="inherit">Login</Button>
+          </Toolbar>
+        </AppBar>
+      </Box>
+      <Drawer 
+        open={drawerOpen}
+        onClose={toggleDrawer(false)}
+      >Hello</Drawer>
+    </>  
   );
 }
