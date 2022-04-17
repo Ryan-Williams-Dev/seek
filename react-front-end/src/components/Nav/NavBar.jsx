8513bd060a222e184fd8f4155b1653e97f931d89
Zenophage <79440234+Zenophage@users.noreply.github.com>
2022-04-16 19:06:22 -0700

    imported material ui drawer, need to refactor to suit our needs

--- react-front-end/src/components/Nav/NavBar.jsx
+++ react-front-end/src/components/Nav/NavBar.jsx
@@ -7,6 +7,10 @@ import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
 
 export default function ButtonAppBar(props) {
+
+  
+
+
   return (
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static">
