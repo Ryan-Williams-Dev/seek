e64f15d53ff51ad0475f26cb7ca1deba97c9724c
Ryan <ryan.maceachern@gmail.com>
2022-04-18 21:27:15 -0700

    create function to calculate distance between game location and guess location

--- express-back-end/helpers/helpers.js
+++ express-back-end/helpers/helpers.js
@@ -1,8 +1,25 @@
 
 const calculateDistance = (gameLocation, guessLocation) => {
-    const { gameLat, gameLng } = gameLocation
-    const { guessLat, guessLng } = guessLocation
+  const { gameLat, gameLng } = gameLocation;
+  const { guessLat, guessLng } = guessLocation;
 
-    const 
+  const radGameLat = Math.PI * gameLat / 180;
+  const radGuessLat = Math.PI * guessLat / 180;
+  const theta = gameLng - guessLng;
 
+  const radTheta = Math.PI * theta / 180;
+  let distance = Math.sin(radGameLat) *  Math.sin(radGuessLat) + Math.cos(radGameLat) * Math.cos(radGuessLat) * Math.cos(radTheta);
+
+  if (distance > 1) {
+    distance = 1;
+  }
+
+  distance = Math.acos(distance);
+  distance = distance * 180 / Math.PI;
+  distance = distance * 60 * 1.1515;
+
+  // Converts distance from miles to kilometers.
+  distance = distance * 1.609344;
+
+  return distance;
 }
