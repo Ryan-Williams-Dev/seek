aa9ea05272afb21f872053db6b0cbab2526279e4
Ryan <ryan.maceachern@gmail.com>
2022-04-19 15:44:45 -0700

    calculateDistance function now works as expected

--- express-back-end/helpers/helpers.js
+++ express-back-end/helpers/helpers.js
@@ -6,7 +6,7 @@ const calculateDistance = (gameLocation, guessLocation) => {
   const gameLat = gameLocation.latitude;
   const gameLng = gameLocation.longitude;
   const guessLat = guessLocation.lat;
-  const guessLng = guessLocation.lat;
+  const guessLng = guessLocation.lng;
 
   const radGameLat = Math.PI * gameLat / 180;
   const radGuessLat = Math.PI * guessLat / 180;
@@ -14,11 +14,10 @@ const calculateDistance = (gameLocation, guessLocation) => {
 
   const radTheta = Math.PI * theta / 180;
   let distance = Math.sin(radGameLat) * Math.sin(radGuessLat) + Math.cos(radGameLat) * Math.cos(radGuessLat) * Math.cos(radTheta);
-
   if (distance > 1) {
     distance = 1;
   }
-
+  
   distance = Math.acos(distance);
   distance = distance * 180 / Math.PI;
   distance = distance * 60 * 1.1515;
@@ -51,7 +50,7 @@ const calculateScore = (distance) => {
     score = distance * 2;
   } else if (distance >= 0.5 && distance < 1) {
     score = distance * 3;
-  } else if (distance >= 0.15 && distance < 0.5 ) {
+  } else if (distance >= 0.15 && distance < 0.5) {
     score = distance * 3.5;
   } else if (distance < 0.15) {
     score = distance * 4;
@@ -60,10 +59,27 @@ const calculateScore = (distance) => {
   return score;
 };
 
-const game = {};
-const guess = {lat: 51.70663210602697, lng: 67.90055248618782,};
-
-console.log("calculateDistance:", calculateDistance(game, guess))
+// const calculateDistance = (gameLocation, guessLocation) => {
+//   let lat1 = gameLocation.latitude;
+//   let lon1 = gameLocation.longitude;
+//   let lat2 = guessLocation.lat;
+//   let lon2 = guessLocation.lng;
+  
+//   let R = 6371; // km
+//   let dLat = toRad(lat2 - lat1);
+//   let dLon = toRad(lon2 - lon1);
+//   lat1 = toRad(lat1);
+//   lat2 = toRad(lat2);
+//   let a = Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+//     Math.sin(dLon / 2) * Math.sin(dLon / 2) * Math.cos(lat1) * Math.cos(lat2);
+//   let c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+//   let d = R * c;
+//   return d;
+// };
+// // Converts numeric degrees to radians
+// const toRad = (Value) => {
+//   return Value * Math.PI / 180;
+// };
 
 
 module.exports = { calculateDistance, calculateScore };
