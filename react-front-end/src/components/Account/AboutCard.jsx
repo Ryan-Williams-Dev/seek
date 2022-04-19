670fd5a7a4987c97618015a8dc20af4feb5de333
Ryan <ryan.maceachern@gmail.com>
2022-04-18 18:22:11 -0700

    create basic profile card

--- react-front-end/src/components/Account/AboutCard.jsx
+++ react-front-end/src/components/Account/AboutCard.jsx
@@ -14,9 +14,14 @@ const AboutCard = () => {
         <Typography gutterBottom variant="h4">
           Zarah Liao
         </Typography>
-        <Typography>
-          
-        </Typography>
+        <ul className='about-list'>
+          <dt><strong>Email</strong></dt>
+          <dd>zarah101@hotmail.com</dd>
+          <dt><strong>Location</strong></dt>
+          <dd>Vancouver, BC, Canada</dd>
+          <dt><strong>Streak</strong></dt>
+          <dd>5</dd>
+        </ul>
       </CardContent>
     </Card>
   );
