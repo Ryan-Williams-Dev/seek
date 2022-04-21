6131fcf8e27656657385ea3d7a352112a2fda9b2
Ryan <ryan.maceachern@gmail.com>
2022-04-21 14:05:38 -0700

    rounded edges on popup window. 'Create Another Game' now refreshes page

--- react-front-end/src/components/Popup/CreateGamePopup.jsx
+++ react-front-end/src/components/Popup/CreateGamePopup.jsx
@@ -2,14 +2,18 @@ import './popup-styles.scss'
 import { Button } from '@mui/material';
 
 const CreateGamePopup = (props) => {
+  const refreshPage = () => {
+    window.location.reload(false);
+  };  
+  
   return (props.trigger) ? (
     <div className="popup">
       <div className="popup-inner">
-        <h3>Game Saved!</h3>
+        <h2>Game Saved!</h2>
         <div className='btn-row'>
-        <Button className="popup-btn">Challenge A Friend</Button>
-        <Button className="popup-btn">My Games</Button>
-        <Button className="popup-btn">Create Another Game</Button>
+          <Button className="popup-btn">Challenge A Friend</Button>
+          <Button className="popup-btn" href="">My Games</Button>
+          <Button className="popup-btn" onClick={refreshPage}>Create Another Game</Button>
         </div>
         { props.children }
       </div>
