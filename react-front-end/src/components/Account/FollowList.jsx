8578b3e60bdb8ef6fe35b6785ac16e0e7c159771
Ryan <ryan.maceachern@gmail.com>
2022-04-21 21:39:36 -0700

    refactoring and linting

--- react-front-end/src/components/Account/FollowList.jsx
+++ react-front-end/src/components/Account/FollowList.jsx
@@ -12,8 +12,8 @@ const FollowList = () => {
       <Table sx={{minWidth: 300}} size="small" aria-label="a dense table">
         {/* map multiple FollowListItem components here based on follows database table */}
         <FollowListItem initials="RM" name="Ryan MacEachern" />
-        <FollowListItem initials="RW" name="Ryan Williams"/>
-        <FollowListItem initials="HM" name="Hannah Montana"/>
+        <FollowListItem initials="RW" name="Ryan Williams" />
+        <FollowListItem initials="HM" name="Hannah Montana" />
       </Table>
       </CardContent>
     </Card>
--- react-front-end/src/pages/Account.jsx
+++ react-front-end/src/pages/Account.jsx
@@ -9,20 +9,16 @@ const Account = () => {
   
   const { user } = useContext(authContext);
   const [userData, setUserData] = useState({})
-  // localhost:3000/api?param1=something&param2=other
-  // base_URL/users?user_id=5
-  // new route => base_URL/users/{user_id}
 
   useEffect(() => {
     console.log("USERID:", user.id)
-    axios.get(process.env.REACT_APP_API_BASE_URL + `users/${user.id}`)
+    axios.get(`${process.env.REACT_APP_API_BASE_URL}users/${user.id}`)
       .then(res => {
         console.log("Request successful.");
         setUserData(...res.data);
-        console.log("USERDATA:", userData);
       })
       .catch(err => {
-        console.log("Error:", err)
+        console.log("Error:", err);
       });
   }, []);
 
@@ -32,12 +28,12 @@ const Account = () => {
     <>
       <div className='profile-header'>
         <div className='about-card'>
-          <AboutCard />
+          <AboutCard userData={userData}/>
         </div>
       </div>
       <div className='info-row'>
-        <Stats />
-        <FollowList />
+        <Stats userData={userData}/>
+        <FollowList userData={userData}/>
       </div>
     </>
   );
