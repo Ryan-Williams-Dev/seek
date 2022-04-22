6c1384544c102fe220de67f44baf5c55ba7ce4ff
Ryan <ryan.maceachern@gmail.com>
2022-04-22 12:57:01 -0700

    added guesses seed for our ryanm@email.com test user. altered db query to grab all data relevant to account page

---
+++ express-back-end/db/seeds/06_guesses.sql
@@ -0,0 +1,19 @@
+INSERT INTO guesses (
+  game_id,
+  user_id,
+  latitude,
+  longitude,
+  score
+) VALUES (
+  1,
+  3,
+  50.6843209,
+  -2.5043278,
+  4365
+), (
+  2,
+  3,
+  54.56498,
+  12.23794,
+  3835
+)
--- express-back-end/routes/users.js
+++ express-back-end/routes/users.js
@@ -6,7 +6,12 @@ module.exports = (db) => {
   // retrieve data for specific user for Account page
   router.get('/:id', (req, res) => {
     const userID = req.params.id;
-    return db.query("SELECT * FROM users WHERE id = $1;", [userID])
+    return db.query(
+      `SELECT first_name, last_name, email, avatar_url, followed_id AS following, SUM(score) AS total_score FROM users
+      JOIN follows ON users.id = user_id
+      JOIN guesses ON users.id = guesses.user_id
+      WHERE users.id = $1
+      GROUP BY first_name, last_name, email, avatar_url, followed_id;`, [userID])
       .then((data) => {
         console.log("Sending user data...");
         res.send(data.rows);
