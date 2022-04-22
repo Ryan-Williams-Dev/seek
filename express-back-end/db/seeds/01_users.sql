b60d0d6f73f5e50eb8777c301d67fa8145e1116d
Ryan <ryan.maceachern@gmail.com>
2022-04-21 17:04:45 -0700

    update user db seeds to add more users

--- express-back-end/db/seeds/01_users.sql
+++ express-back-end/db/seeds/01_users.sql
@@ -17,9 +17,49 @@ INSERT INTO users (
 ), (
   'user2',
   'fake@email.com',
-  'testpass',
+  'test',
   'user',
   '2',
   'image2.com',
   'Hello'
-);
+), (
+  'ryanmaceachern',
+  'ryanm@email.com',
+  'test',
+  'Ryan',
+  'MacEachern',
+  '../../lib/images/ryanm.jpg',
+  'Old enough to party.'
+), (
+  'ryan-williams',
+  'ryanw@geeemail.com',
+  'test',
+  'Ryan',
+  'Williams',
+  '../../lib/images/ryanw.jpg',
+  'Greatest of All Time.'
+), (
+  'z-lo',
+  'zarahliao@email.com',
+  'test',
+  'Zarah',
+  'Liao',
+  '../../lib/images/avatar1.jpg',
+  'Love puzzles!'
+) (
+  'ramrod',
+  'ramrod@email.com',
+  'test',
+  'Rodney',
+  'Mullet',
+  '../../lib/images/avatar2.jpg',
+  'Go big or go home'
+), (
+  'libby499',
+  'elle@email.com',
+  'test',
+  'Elizabeth',
+  'Proud',
+  '../../lib/images/avatar3.jpg',
+  'Ready to roll!'
+) 
