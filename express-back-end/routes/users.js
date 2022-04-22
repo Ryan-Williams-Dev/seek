6f1be530bb92e7455975292d5b6d1aba79e02b21
Ryan <ryan.maceachern@gmail.com>
2022-04-21 18:06:08 -0700

    add semi colons to code

--- express-back-end/routes/users.js
+++ express-back-end/routes/users.js
@@ -1,4 +1,4 @@
-const express = require('express')
+const express = require('express');
 const router = express.Router();
 
 module.exports = (db) => {
@@ -7,17 +7,17 @@ module.exports = (db) => {
     const { email, password } = req.body;
     return db.query("SELECT * FROM users WHERE email = $1;", [email])
       .then((data) => {
-        user = data.rows[0]
+        user = data.rows[0];
         if (password !== user.password_digest) {
-          return res.send({valid: false})
+          return res.send({valid: false});
         }
-        return res.send({valid: true, user})
+        return res.send({valid: true, user});
       })
       .catch((err) => {
-        console.log(err)
-        if (err) return res.send({valid: false, error: err})
-      })
-  })
+        console.log(err);
+        if (err) return res.send({valid: false, error: err});
+      });
+  });
 
   return router;
 };
