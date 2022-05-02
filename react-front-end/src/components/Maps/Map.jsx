992e653c9110bf5217f48f02098a380521cb68a9
Ryan MacEachern <ryan.maceachern@gmail.com>
2022-05-02 10:22:18 -0400

    inserting console logs for Polyline

--- react-front-end/src/components/Maps/Map.jsx
+++ react-front-end/src/components/Maps/Map.jsx
@@ -63,6 +63,7 @@ export default function Map(props) {
       setView(props.result.answer, setCenter, mapRef)
       hasPlacedAnswer.current = true;
       // setLine();
+      console.log("Polyline:", Polyline)
     }
   }, [props.result, markers, isLoaded]);
   
