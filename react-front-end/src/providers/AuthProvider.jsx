c2f27b2d05901f8f30f337fd35b9306c9a6f8673
Zenophage <79440234+Zenophage@users.noreply.github.com>
2022-05-05 12:26:25 -0700

    created admin route and added admin functionality to authContext

--- react-front-end/src/providers/AuthProvider.jsx
+++ react-front-end/src/providers/AuthProvider.jsx
@@ -1,14 +1,18 @@
 import { createContext, useState } from 'react';
-import Cookies from 'js-cookie'
+import Cookies from 'js-cookie';
+import axios from 'axios';
 
 export const authContext = createContext();
 
 export default function AuthProvider(props) {
   const [auth, setAuth] = useState(false);
   const [user, setUser] = useState(null);
+  const [admin, setAdmin] = useState(false)
 
   // Perform login process for the user & save authID
-  const login = function(user) {
+  const login = async function(user) {
+    const isAdmin = await adminCheck(user);
+    if (isAdmin) setAdmin(true);
     setAuth(true);
     setUser(user);
   };
@@ -16,11 +20,17 @@ export default function AuthProvider(props) {
   const logout = function() {
     setAuth(false);
     setUser(null);
+    setAdmin(false);
     Cookies.remove('userId')
     window.location.reload(false);
   };
 
-  const userData = { auth, user, login, logout };
+  const adminCheck = async function(user) {
+    const data  = await axios.get('api/admin', {params: {userId: user.id}})
+    console.log(data)
+  }
+
+  const userData = { auth, user, admin, login, logout };
 
   return (
     <authContext.Provider value={userData}>
