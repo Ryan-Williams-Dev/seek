61b3e22171bba38118c0f1610a21ae3a61897ef2
Zenophage <79440234+Zenophage@users.noreply.github.com>
2022-06-27 14:18:10 -0700

    resstting date after merging in warning for no game

--- express-back-end/helpers/helpers.js
+++ express-back-end/helpers/helpers.js
@@ -35,7 +35,7 @@ const calculateScore = (distance) => {
 };
 
 const generateDailyGameNum = () => {
-  const startDate = new Date(2022, 3, 27); // month is by index (so april = 3), setting startDay to today will result in id = 1;
+  const startDate = new Date(2022, 5, 27); // month is by index (so april = 3), setting startDay to today will result in id = 1;
   const todayDate = new Date();
   const differenceInTime = Math.abs(startDate - todayDate);
   const differenceInDays = Math.ceil(differenceInTime / (1000 * 3600 * 24));
