b0a259ab6878c9003c9ca39b0acf6ed1b8abea1e
Ryan Williams <r.d.williams.ca@gmail.com>
2023-07-19 12:42:18 -0700

    reset date

--- express-back-end/helpers/helpers.js
+++ express-back-end/helpers/helpers.js
@@ -8,18 +8,20 @@ const calculateDistance = (gameLocation, guessLocation) => {
   const guessLat = guessLocation.lat;
   const guessLng = guessLocation.lng;
 
-  const radGameLat = Math.PI * gameLat / 180;
-  const radGuessLat = Math.PI * guessLat / 180;
+  const radGameLat = (Math.PI * gameLat) / 180;
+  const radGuessLat = (Math.PI * guessLat) / 180;
   const theta = gameLng - guessLng;
 
-  const radTheta = Math.PI * theta / 180;
-  let distance = Math.sin(radGameLat) * Math.sin(radGuessLat) + Math.cos(radGameLat) * Math.cos(radGuessLat) * Math.cos(radTheta);
+  const radTheta = (Math.PI * theta) / 180;
+  let distance =
+    Math.sin(radGameLat) * Math.sin(radGuessLat) +
+    Math.cos(radGameLat) * Math.cos(radGuessLat) * Math.cos(radTheta);
   if (distance > 1) {
     distance = 1;
   }
-  
+
   distance = Math.acos(distance);
-  distance = distance * 180 / Math.PI;
+  distance = (distance * 180) / Math.PI;
   distance = distance * 60 * 1.1515;
 
   // Converts distance from miles to kilometers.
@@ -30,16 +32,16 @@ const calculateDistance = (gameLocation, guessLocation) => {
 
 const calculateScore = (distance) => {
   if (distance > 2500) return 0;
-  let score = 5000 - (distance * 2);
+  let score = 5000 - distance * 2;
   return Math.round(score);
 };
 
 const generateDailyGameNum = () => {
-  const startDate = new Date(2022, 9, 21); // month is by index (so april = 3), setting startDay to today will result in id = 1;
+  const startDate = new Date(2023, 6, 19); // month is by index (so april = 3), setting startDay to today will result in id = 1;
   const todayDate = new Date();
   const differenceInTime = Math.abs(startDate - todayDate);
   const differenceInDays = Math.ceil(differenceInTime / (1000 * 3600 * 24));
-  return differenceInDays; 
-}
+  return differenceInDays;
+};
 
 module.exports = { calculateDistance, calculateScore, generateDailyGameNum };
